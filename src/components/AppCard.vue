8ac1393a091d182f1b1d3f9772d0c7e581fbb322
LorenzoEmanuele00 <lorenzoema2800@gmail.com>
2023-11-30 16:53:32 +0100

    add flags and rounded number (true)

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -5,9 +5,22 @@ export default {
     },
     data() {
         return {
-            
+            flags: ["it", "en", "fr", "es", "ja"]
         };
     },
+    methods: {
+        getImagePath(language) {
+            return new URL (`../assets/img/${language}.png`, import.meta.url).href;
+        }
+    },
+    computed: {
+        hasFlag() {
+            return this.flags.includes(this.filmObject.original_language);
+        },
+        roundedNumber() {
+            return Math.floor(this.filmObject.vote_average);
+        },
+    }
 }
 </script>
 
@@ -15,11 +28,17 @@ export default {
     <div class="card">
         <h2>{{ filmObject.title ? filmObject.title : filmObject.name }}</h2>
         <h3>{{ filmObject.original_title ? filmObject.original_title : filmObject.original_name }}</h3>
-        <p>{{ filmObject.original_language }}</p>
-        <p>{{ filmObject.vote_average }}</p>
+        <p v-if="!hasFlag">{{ filmObject.original_language }}</p>
+        <img v-else :src="getImagePath(filmObject.original_language)" alt="">
+        <p>{{ roundedNumber }}</p>
     </div>
 </template>
 
 <style lang="scss" scoped>
-
+    img {
+        width: 80px;
+        aspect-ratio: 2 / 1;
+        border: 1px solid black;
+        border-radius: 10px;
+    }
 </style>
